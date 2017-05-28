53d7361c80b3bed58e0471cd14405d95348b55b1
Aurelien Callens <aurelien@infinitix.io>
2017-05-28 22:04:32 +0200

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,10 @@
 const React = require("react");
 
-var Examples = props => <h3>Examples Component</h3>;
+var Examples = props => (
+<div>
+    <h3>Examples Component</h3>
+    <p>Welcome to Examples page!</p>
+</div>
+);
 
 module.exports = Examples;
