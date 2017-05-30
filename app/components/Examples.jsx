da5f8988fa15133bba01a8f05e9a4dee607471d1
Aurelien Callens <aurelien@infinitix.io>
2017-05-30 20:36:26 +0200

    Add Foundation styling to Main and Examples components

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,18 @@
 const React = require("react");
+const {Link} = require("react-router");
 
 var Examples = props => (
 <div>
-    <h3>Examples</h3>
-    <p>Welcome to Examples page!</p>
+    <h1 className="text-center">Examples</h1>
+    <p>Here a few example locations to try out:</p>
+    <ol>
+        <li>
+            <Link to="/?location=Mons">Mons, Belgium</Link>
+        </li>
+        <li>
+            <Link to="/?location=Rio">Rio, Brazil</Link>            
+        </li>
+    </ol>
 </div>
 );
 
